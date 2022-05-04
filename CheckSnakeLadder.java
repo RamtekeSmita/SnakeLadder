4061aa30f3e129cc19ea0b7ff4a857fda97e0549
Smita <smitaramteke5@gmail.com>
2022-05-04 21:58:20 +0530

    Added: Snake and ladder

---
+++ CheckSnakeLadder.java
@@ -0,0 +1,25 @@
+package com.Bridgelabz.SnakeLadder;
+
+public class CheckSnakeLadder {
+    public static final int Ladder=1;
+    public static final int snake=2;
+    public static void main(String[] args) {
+        int currentposition=0,selfposition=0;
+        int rollDie=(int) Math.floor(Math.random()*10)%7;
+        if(rollDie==Ladder)
+        {
+            currentposition=selfposition+rollDie;
+            System.out.println("Player1 postion:"+currentposition);
+        }
+        else if(rollDie==snake)
+        {
+            currentposition=selfposition-rollDie;
+            System.out.println("Player1 postion:"+currentposition);
+        }
+        else
+        {
+            currentposition=selfposition;
+            System.out.println("No Play");
+        }
+    }
+}
---
+++ SnakeLadder.java
@@ -0,0 +1,10 @@
+package com.Bridgelabz.SnakeLadder;
+
+public class SnakeLadder {
+    public static void main(String[] args) {
+        int player1;
+        int selfposition=0;
+        System.out.println("player1 position:"+selfposition);
+
+    }
+}
